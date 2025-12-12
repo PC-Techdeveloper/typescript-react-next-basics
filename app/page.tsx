3187ae14f5b8a7225307a14483ce4862bd1a7320
nexuscomputo <soportenexuscomputo@hotmail.com>
2025-12-12 13:26:14 -0500

    record types, union types, css properties, destructuring

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,29 @@
+//union types
+type Color = 'red' | 'blue' | 'green' | 'yellow' | 'purple'
+//props
 type ButtonProps = {
   text: string
   subtitle?: string
-  color?: string
+  color?: Color
+  backgroundColor?: Color
 }
+
+//arrays
+type ButtonArrayProps = {
+  padding: [number, number, number]
+}
+
+//css properties: propiedades de CSS
+type ButtonCSSProps = {
+  style: React.CSSProperties
+}
+
+//record types
+type UserAges = {
+  userAges: Record<'Alice' | 'Bob' | 'Jose', number>
+}
+
+//type functions
 
 /* 
 const Button: React.FC<ButtonProps> = (props) => {
@@ -14,23 +35,20 @@ const Button: React.FC<ButtonProps> = (props) => {
 }
 */
 
-function Button(props: ButtonProps) {
-  const { text, subtitle, color } = props
-
-  return (
-    <button>
-      {text}
-      <br />
-      {subtitle}
-      <br />
-    </button>
-  )
+function Button({}: UserAges) {
+  return <button>test</button>
 }
 
-const Page = () => {
+function Page() {
   return (
     <div>
-      <Button text="Hello" subtitle="second text" color="red" />
+      <Button
+        userAges={{
+          Alice: 20,
+          Bob: 30,
+          Jose: 40,
+        }}
+      />
     </div>
   )
 }
