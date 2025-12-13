75540f981c09755937c4a6ced23d2333f9e4563a
nexuscomputo <soportenexuscomputo@hotmail.com>
2025-12-12 21:45:29 -0500

    interfaces, alias, types, function type, etc

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+//indica que el componente es del lado del cliente
+'use client'
+import { ComponentProps, Dispatch, JSX, SetStateAction, useState } from 'react'
+
 //union types
 type Color = 'red' | 'blue' | 'green' | 'yellow' | 'purple'
 //props
@@ -24,6 +28,73 @@ type UserAges = {
 }
 
 //type functions
+type ButtonFunctionProps = {
+  onClick: () => void //no retorna nada
+}
+
+//reactNode vs JSX.Element
+//reactNode ejecuta cualquier tipo de elemento
+// JSX.Element solo ejecuta elementos de React
+type ButtonChildrenProps = {
+  children: JSX.Element | React.ReactNode
+}
+
+//uso de setters
+type ButtonSetterProps = {
+  setCount: Dispatch<SetStateAction<number>>
+  children: React.ReactNode
+}
+
+//default props
+type ButtonDefaultProps = {
+  title?: string
+  count?: number
+}
+
+//alias vs interfaces
+interface ButtonPropsInterface {
+  title?: string
+  count?: number
+}
+
+type Domain = string
+/* ❌ */
+// interface Domain = string
+interface MyDomain {
+  domain: string
+}
+//tipado de props en componentes
+type ButtonPropsComponent = ComponentProps<'button'>
+
+//intersection y extends 🟡
+
+/* spread operator */
+function Button({ onClick, ...rest }: ButtonPropsComponent) {
+  // manejador de eventos
+  const handleClick = () => {
+    if (onClick) {
+      alert('clicked')
+    }
+  }
+
+  return (
+    <button onClick={handleClick} {...rest}>
+      Button
+    </button>
+  )
+}
+
+function Page() {
+  // const [count, setCount] = useState<number>(0)
+
+  return (
+    <div>
+      <Button onClick={() => console.log('Alert')}>Button</Button>
+    </div>
+  )
+}
+
+export default Page
 
 /* 
 const Button: React.FC<ButtonProps> = (props) => {
@@ -34,23 +105,3 @@ const Button: React.FC<ButtonProps> = (props) => {
   )
 }
 */
-
-function Button({}: UserAges) {
-  return <button>test</button>
-}
-
-function Page() {
-  return (
-    <div>
-      <Button
-        userAges={{
-          Alice: 20,
-          Bob: 30,
-          Jose: 40,
-        }}
-      />
-    </div>
-  )
-}
-
-export default Page
